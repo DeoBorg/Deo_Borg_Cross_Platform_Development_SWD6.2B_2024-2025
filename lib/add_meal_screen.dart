489cda80a2d6423bb40678e229b2a13837805e11
Deo Borg <deoborg@Deos-MacBook-Pro.local>
2025-03-16 23:07:06 +0100

    Implemented firebase connection to upload meals to firebase including storing an image to local device, and uploading the corresponding UUID to be able to display the image (Not working yet) and also loads meals dynamically from firebase.

--- lib/add_meal_screen.dart
+++ lib/add_meal_screen.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:uuid/uuid.dart';
 
 class AddMealScreen extends StatefulWidget {
   const AddMealScreen({super.key});
@@ -14,14 +18,78 @@ class _AddMealScreenState extends State<AddMealScreen> {
   final TextEditingController caloriesController = TextEditingController();
   final TextEditingController weightController = TextEditingController();
 
-  File? _mealImage;
+  final DatabaseReference mealsRef = FirebaseDatabase.instanceFor(
+    app: Firebase.app(),
+    databaseURL:
+        "https://calorie-snap-4942e-default-rtdb.europe-west1.firebasedatabase.app",
+  ).ref("meals"); // Connects to my Firebase Realtime Database
 
-  Future<void> _pickImage() async {
-    final pickedFile = await ImagePicker().pickImage(source: ImageSource.camera);
+  File? _mealImage;
+  String? _imageUUID; // Will be used to store the image in Firebase Storage
+  bool _isSubmitting = false;
+
+  Future<void> _pickImage() async { //Function to use the camera to take a photo
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.camera);
 
     if (pickedFile != null) {
+      final appDir = await getApplicationDocumentsDirectory();
+      String fileName = const Uuid().v4();
+      final localImage = File('${appDir.path}/$fileName.jpg');
+
+      await File(pickedFile.path).copy(localImage.path);
+
       setState(() {
-        _mealImage = File(pickedFile.path); // Store the taken photo
+        _mealImage = localImage;
+        _imageUUID = fileName;
+      });
+    }
+  }
+
+  bool _isFormValid() {
+    return mealNameController.text.isNotEmpty &&
+        caloriesController.text.isNotEmpty &&
+        weightController.text.isNotEmpty &&
+        _mealImage != null;
+  }
+
+  Future<void> _submitMeal() async { //Function to submit the meal
+    if (!_isFormValid()) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+            content: Text("Please fill in all fields and add a photo")),
+      );
+      return;
+    }
+
+    setState(() {
+      _isSubmitting = true;
+    });
+
+    try {
+      String mealId = mealsRef.push().key!; // Generates a unique key for the meal 
+      await mealsRef.child(mealId).set({
+        "name": mealNameController.text.trim(),
+        "calories": int.parse(caloriesController.text.trim()),
+        "weight": int.parse(weightController.text.trim()),
+        "imageUUID": _imageUUID,
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Meal added successfully")),
+      );
+
+      setState(() {
+        _isSubmitting = false;
+      });
+
+      Navigator.pop(context);
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Error: $e")),
+      );
+      setState(() {
+        _isSubmitting = false;
       });
     }
   }
@@ -29,10 +97,11 @@ class _AddMealScreenState extends State<AddMealScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF8D6E63), 
+      backgroundColor: const Color(0xFF8D6E63),
       appBar: AppBar(
-        title: const Text("Calorie Snap", style: TextStyle(fontWeight: FontWeight.bold)),
-        backgroundColor: const Color(0xFF4B2E2E), 
+        title: const Text("Calorie Snap",
+            style: TextStyle(fontWeight: FontWeight.bold)),
+        backgroundColor: const Color(0xFF4B2E2E),
         centerTitle: true,
       ),
       body: Padding(
@@ -40,19 +109,17 @@ class _AddMealScreenState extends State<AddMealScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // Meal name input Field
             TextField(
               controller: mealNameController,
               decoration: InputDecoration(
                 labelText: "Meal Name",
                 fillColor: Colors.white,
                 filled: true,
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
               ),
             ),
             const SizedBox(height: 15),
-
-            // Calories input field
             TextField(
               controller: caloriesController,
               keyboardType: TextInputType.number,
@@ -60,12 +127,11 @@ class _AddMealScreenState extends State<AddMealScreen> {
                 labelText: "Calories",
                 fillColor: Colors.white,
                 filled: true,
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
               ),
             ),
             const SizedBox(height: 15),
-
-            // Weight input Field
             TextField(
               controller: weightController,
               keyboardType: TextInputType.number,
@@ -73,69 +139,72 @@ class _AddMealScreenState extends State<AddMealScreen> {
                 labelText: "Weight (grams)",
                 fillColor: Colors.white,
                 filled: true,
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
               ),
             ),
             const SizedBox(height: 20),
-
             if (_mealImage != null)
-            Center(
+              Center(
                 child: Column(
                   children: [
-                    Image.file(_mealImage!, height: 150), // Show image
+                    Image.file(_mealImage!, height: 150),
                     const SizedBox(height: 10),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         ElevatedButton(
-                          onPressed: _pickImage,// Retake the photo
-                          style: ElevatedButton.styleFrom(backgroundColor: Colors.redAccent),
-                          child: const Text("Retake", style: TextStyle(color: Colors.white)),
+                          onPressed: _pickImage,
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.redAccent),
+                          child: const Text("Retake",
+                              style: TextStyle(color: Colors.white)),
                         ),
                       ],
                     ),
                   ],
                 ),
               ),
-            // Add Photo Button
             Center(
               child: ElevatedButton.icon(
                 onPressed: _pickImage,
                 icon: const Icon(Icons.camera_alt),
                 label: const Text("Add Photo of Meal"),
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFFD7B899), 
-                
+                  backgroundColor: const Color(0xFFD7B899),
                   foregroundColor: Colors.black,
-                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
                 ),
               ),
             ),
             const SizedBox(height: 30),
-
-            //Add Meal & Cancel
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 ElevatedButton(
-                  onPressed: () {
-                  },
+                  onPressed: _isSubmitting ? null : _submitMeal, 
                   style: ElevatedButton.styleFrom(
                     backgroundColor: const Color(0xFFD7B899),
-                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 12),
                   ),
-                  child: const Text("Add Meal", style: TextStyle(color: Colors.black)),
+                  child: _isSubmitting 
+                      ? const CircularProgressIndicator(color: Colors.black)
+                      : const Text("Add Meal",
+                          style: TextStyle(color: Colors.black)),
                 ),
-
                 ElevatedButton(
                   onPressed: () {
-                    Navigator.pop(context); // Go To Home screen
+                    Navigator.pop(context);
                   },
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.redAccent,
-                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 12),
                   ),
-                  child: const Text("Cancel", style: TextStyle(color: Colors.white)),
+                  child: const Text("Cancel",
+                      style: TextStyle(color: Colors.white)),
                 ),
               ],
             ),
--- lib/meal_selection_screen.dart
+++ lib/meal_selection_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class MealSelectionScreen extends StatefulWidget {
   const MealSelectionScreen({super.key});
@@ -8,125 +10,159 @@ class MealSelectionScreen extends StatefulWidget {
 }
 
 class _MealSelectionScreenState extends State<MealSelectionScreen> {
-  final List<Map<String, dynamic>> meals = [
-    {"name": "Grilled Chicken", "calories": 350, "weight": 200, "image": ""},
-    {"name": "Avocado Toast", "calories": 250, "weight": 150, "image": ""},
-    {
-      "name": "Spaghetti Bolognese",
-      "calories": 600,
-      "weight": 300,
-      "image": ""
-    },
-    {"name": "Salmon with Rice", "calories": 450, "weight": 250, "image": ""},
-    {"name": "Caesar Salad", "calories": 300, "weight": 180, "image": ""},
-    {"name": "Protein Smoothie", "calories": 200, "weight": 250, "image": ""},
-  ];
+  final DatabaseReference mealsRef = FirebaseDatabase.instanceFor(
+          app: Firebase.app(),
+          databaseURL:
+              "https://calorie-snap-4942e-default-rtdb.europe-west1.firebasedatabase.app")
+      .ref("meals");
 
-  int? selectedMealIndex; // Track selected meal index
+  List<Map<String, dynamic>> meals = [];
+  int? selectedMealIndex;
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadMeals();
+  }
+
+  void _loadMeals() {
+    mealsRef.onValue.listen((DatabaseEvent event) {
+      final data = event.snapshot.value as Map<dynamic, dynamic>?;
+
+      if (data != null) {
+        List<Map<String, dynamic>> loadedMeals = [];
+        data.forEach((key, value) {
+          loadedMeals.add({
+            "id": key,
+            "name": value["name"] ?? "Unknown",
+            "calories": value["calories"] ?? 0,
+            "weight": value["weight"] ?? 0,
+            "imageUUID": value["imageUUID"] ?? "",
+          });
+        });
+
+        setState(() {
+          meals = loadedMeals;
+          isLoading = false;
+        });
+      } else {
+        setState(() {
+          meals = [];
+          isLoading = false;
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF8D6E63),
       appBar: AppBar(
-        title:
-            const Text("Calorie Snap", style: TextStyle(fontWeight: FontWeight.bold)),
+        title: const Text("Calorie Snap",
+            style: TextStyle(fontWeight: FontWeight.bold)),
         backgroundColor: const Color(0xFF4B2E2E),
         centerTitle: true,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Expanded(
-              child: GridView.builder(
-                itemCount: meals.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                  childAspectRatio: 0.9,
-                ),
-                itemBuilder: (context, index) {
-                  bool isSelected =
-                      selectedMealIndex == index; //check if selected
-
-                  return GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        selectedMealIndex = index;
-                      });
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: isSelected
-                            ? const Color(0xFFC69C6D)
-                            : Colors.grey[300], // Highlight selection
-                        borderRadius: BorderRadius.circular(12),
-                        border: isSelected
-                            ? Border.all(
-                                color: Colors.black,
-                                width: 2) // Add border when selected
-                            : null,
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Icon(Icons.fastfood,
-                              size: 50, color: Colors.brown), // Temporary icon
-                          const SizedBox(height: 8),
-                          Text(
-                            meals[index]["name"], // Meal Name
-                            style: const TextStyle(
-                                fontSize: 14, fontWeight: FontWeight.bold),
+        child: isLoading
+            ? const Center(child: CircularProgressIndicator()) // Loading state
+            : meals.isEmpty
+                ? const Center(child: Text("No meals found"))
+                : Column(
+                    children: [
+                      Expanded(
+                        child: GridView.builder(
+                          itemCount: meals.length,
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            crossAxisSpacing: 10,
+                            mainAxisSpacing: 10,
+                            childAspectRatio: 0.9,
                           ),
-                          Text(
-                            "${meals[index]["calories"]} cal | ${meals[index]["weight"]}g",
-                            style: const TextStyle(
-                                fontSize: 12, fontWeight: FontWeight.w500),
+                          itemBuilder: (context, index) {
+                            bool isSelected = selectedMealIndex == index;
+
+                            return GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  selectedMealIndex = index;
+                                });
+                              },
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: isSelected
+                                      ? const Color(0xFFC69C6D)
+                                      : Colors.grey[300],
+                                  borderRadius: BorderRadius.circular(12),
+                                  border: isSelected
+                                      ? Border.all(
+                                          color: Colors.black, width: 2)
+                                      : null,
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    const Icon(Icons.fastfood,
+                                        size: 50, color: Colors.brown),
+                                    const SizedBox(height: 8),
+                                    Text(
+                                      meals[index]["name"],
+                                      style: const TextStyle(
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      "${meals[index]["calories"]} cal | ${meals[index]["weight"]}g",
+                                      style: const TextStyle(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          ElevatedButton(
+                            onPressed: selectedMealIndex != null
+                                ? () {
+                                    Navigator.pop(
+                                        context, meals[selectedMealIndex!]);
+                                  }
+                                : null,
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xFFD7B899),
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 12),
+                            ),
+                            child: const Text("Add Meal",
+                                style: TextStyle(color: Colors.black)),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.redAccent,
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 12),
+                            ),
+                            child: const Text("Cancel",
+                                style: TextStyle(color: Colors.white)),
                           ),
                         ],
                       ),
-                    ),
-                  );
-                },
-              ),
-            ),
-
-            const SizedBox(height: 10),
-
-            // Bottom Buttons
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                ElevatedButton(
-                  onPressed: selectedMealIndex != null
-                      ? () {
-                          // Return the selected meal data to the HomeScreen
-                          Navigator.pop(context, meals[selectedMealIndex!]);
-                        }
-                      : null, // Disable button if no meal is selected
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFFD7B899),
-                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                    ],
                   ),
-                  child:
-                      const Text("Add Meal", style: TextStyle(color: Colors.black)),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context); // Go back to Home Screen
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.redAccent,
-                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-                  ),
-                  child: const Text("Cancel", style: TextStyle(color: Colors.white)),
-                ),
-              ],
-            ),
-          ],
-        ),
       ),
     );
   }
